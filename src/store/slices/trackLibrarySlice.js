3931cb6dd6fd13180cdc125a9ce5ee3850ed9c9e
Gardly Philoctete <gardly.philoctete@gmail.com>
2026-02-02 23:55:27 -0500

    more test

--- src/store/slices/trackLibrarySlice.js
+++ src/store/slices/trackLibrarySlice.js
@@ -128,7 +128,7 @@ export const fetchLibrariesWithTracks = createAsyncThunk(
                     },
                   }));
                   console.log('✅ Fetched track license options:', trackLicenseOptions);
-                  console.log('✅ Fetched track license options:', licensingOptions.track_storage_file.file_format.name);
+                  console.log('✅ Fetched STORAGE FILE:', licensingOptions);
                   // Find the Sample license option (use .find() not .filter()) because there is only one sample license option per track and find() returns the first match whereas filter() returns an array of all matches
                   const targetFormat = trackLibrary.libraryName === "FEATURES" ? "SONG" : "SAMPLE"; // if library is Features, use the SONG name, else use the Sample name0 for beats library
                   console.log('✅ Fetched track target format:', targetFormat);
